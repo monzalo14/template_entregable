7a973f60028628ea4436cd21c412c126a345b60f
monzalo14 <monzalo14@gmail.com>
2019-01-22 11:51:03 -0600

    Refined compilation script, added few details to chapters

--- compile.sh
+++ compile.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# Horrible fix for viewing pdf output in a regular PDF Viewer
+output_path='/mnt/c/Users/Monica/Desktop/tercer_entregable.pdf'
+
 pdflatex main.tex
-bibtext main
+bibtex main
 pdflatex main.tex
 pdflatex main.tex
+cp main.pdf $output_path
+echo "Copied to:"$output_path
